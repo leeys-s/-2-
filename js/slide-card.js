083e42ca487d6752b4942366541b1271c6f78364
LJH <dlwnsguq2002@gmail.com>
2024-11-07 14:08:27 +0900

    JS.slide 작업 11월7일오전수업

---
+++ js/slide-card.js
@@ -0,0 +1,8 @@
+// // 11월 7일(목) 작업 html 379라인
+
+window.addEventListener("load", function () {
+  const cardSlide = new Swiper(".mb-card-slide-wrap", {
+    slidesPerView: 4,
+    spaceBetween: "20px",
+  });
+});
---
+++ js/slide-main.js
@@ -0,0 +1,4 @@
+// 11월 7일(목) 작업 html 104라인
+window.addEventListener("load", function () {
+  const vissualSlide = new Swiper(".visual-slide", { loop: true });
+});
