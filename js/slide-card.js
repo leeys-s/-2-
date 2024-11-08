68e5029ec97c0adeaf2c6c6c55f105745ed1395a
LJH <dlwnsguq2002@gmail.com>
2024-11-08 14:04:01 +0900

    2주차 11월 8일(금) JS 학습

--- js/slide-card.js
+++ js/slide-card.js
@@ -1,8 +1,10 @@
-// // 11월 7일(목) 작업 html 379라인
+// 11월 7일(목) 작업 html 379라인
 
 window.addEventListener("load", function () {
   const cardSlide = new Swiper(".mb-card-slide-wrap", {
     slidesPerView: 4,
-    spaceBetween: "20px",
+    spaceBetween: 20,
+    loop: true,
   });
 });
+// main-slide.css 81줄 .mb-card-slide-wrap
